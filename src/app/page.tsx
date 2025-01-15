fa2a08bf5efb24e9ae569410394b9971a03014a9
yekta <ngyekta@gmail.com>
2025-01-15 07:58:56 +0300

    Padding adjustment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SearchBox from "@/components/search-box";
 export default async function Home() {
   return (
     <div className="w-full flex-1 flex flex-col items-center">
-      <div className="w-full max-w-7xl px-4 md:px-8 flex-1 flex flex-col justify-center items-center pt-4 pb-[calc(6vh+2rem)]">
+      <div className="w-full max-w-7xl px-4 md:px-8 flex-1 flex flex-col justify-center items-center pt-4 pb-[calc(10vh+2rem)]">
         <div className="w-full flex flex-col items-center">
           <div className="w-full flex items-center justify-center px-4">
             <Logo variant="full" className="w-32 max-w-full h-auto" />
