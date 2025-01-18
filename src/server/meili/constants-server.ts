3f0532885ad83a4ad56ea2c4a9a877c88e62eb27
yekta <ngyekta@gmail.com>
2025-01-18 11:18:17 +0300

    meiliInternal

--- src/server/meili/constants-server.ts
+++ src/server/meili/constants-server.ts
@@ -7,3 +7,8 @@ export const meiliAdmin = new MeiliSearch({
   host: env.MEILI_URL_INTERNAL,
   apiKey: env.MEILI_ADMIN_KEY,
 });
+
+export const meiliInternal = new MeiliSearch({
+  host: env.MEILI_URL_INTERNAL,
+  apiKey: env.NEXT_PUBLIC_MEILI_CLIENT_KEY,
+});
