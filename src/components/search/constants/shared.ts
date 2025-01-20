638c3c91831a054d5b281e96ea0f8712ae074b49
yekta <ngyekta@gmail.com>
2025-01-20 16:42:44 +0300

    Bulk limit 15K

--- src/components/search/constants/shared.ts
+++ src/components/search/constants/shared.ts
@@ -1,6 +1,3 @@
-export const OFFSET_DEFAULT = 0;
-export const LIMIT_DEFAULT = 50;
-export const LIMIT_BULK = 20_000;
 export const PAGE_DEFAULT = 1;
 export const HITS_PER_PAGE_DEFAULT = 50;
-export const HITS_PER_PAGE_BULK = 20_000;
+export const HITS_PER_PAGE_BULK = 15_000;
