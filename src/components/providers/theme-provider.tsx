8c747461b7351805f11cfed534bb3dda6765ac58
yekta <ngyekta@gmail.com>
2025-01-20 13:53:13 +0300

    Fix theme

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import * as React from "react";
+import {
+  defaultTheme,
+  metaTheme,
+  themes,
+  TThemeWithoutSystem,
+} from "@/components/providers/themes";
 import {
   ThemeProvider as NextThemesProvider,
   ThemeProviderProps,
+  useTheme,
 } from "next-themes";
-import { defaultTheme, themes } from "@/components/providers/themes";
+import * as React from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
@@ -16,7 +22,22 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
       disableTransitionOnChange
       {...props}
     >
-      {children}
+      <ThemeProviderInner>{children}</ThemeProviderInner>
     </NextThemesProvider>
   );
+}
+
+function ThemeProviderInner({ children }: { children: React.ReactNode }) {
+  const { theme, systemTheme } = useTheme();
+  const finalTheme: TThemeWithoutSystem =
+    (theme === "system" ? systemTheme : (theme as TThemeWithoutSystem)) ||
+    defaultTheme;
+
+  React.useEffect(() => {
+    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute("content", metaTheme[finalTheme]);
+    }
+  }, [finalTheme]);
+  return children;
 }
--- src/components/providers/themes.ts
+++ src/components/providers/themes.ts
@@ -1,4 +1,10 @@
 export const availableThemes = ["light", "dark", "system"] as const;
 export const themes = [...availableThemes];
 export type TTheme = (typeof availableThemes)[number];
-export const defaultTheme: TTheme = "light";
+export type TThemeWithoutSystem = Exclude<TTheme, "system">;
+export const defaultTheme: TThemeWithoutSystem = "light";
+
+export const metaTheme: Record<TThemeWithoutSystem, string> = {
+  dark: "#09080d",
+  light: "#ffffff",
+};
