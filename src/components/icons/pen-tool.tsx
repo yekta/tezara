917104e5f394eec74f19040dc41cf16c96c28ad0
yekta <ngyekta@gmail.com>
2025-01-21 13:03:55 +0300

    Fix pen tool icon

--- src/components/icons/pen-tool.tsx
+++ src/components/icons/pen-tool.tsx
@@ -15,7 +15,7 @@ export default function PenToolIcon(props: ComponentProps<"svg">) {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
-        d="M8 2v4m8-4v4M3 10h18M5 4h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2Z"
+        d="m18 13-1.38-6.87a1 1 0 0 0-.74-.78L3.24 2.03a1 1 0 0 0-1.21 1.2l3.32 12.65a1 1 0 0 0 .78.74L13 18M2.3 2.3l7.29 7.29m6.12 11.7a1 1 0 0 1-1.42 0l-1.58-1.58a1 1 0 0 1 0-1.42l5.58-5.58a1 1 0 0 1 1.42 0l1.58 1.58a1 1 0 0 1 0 1.42l-5.58 5.58ZM13 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
       />
     </svg>
   );
