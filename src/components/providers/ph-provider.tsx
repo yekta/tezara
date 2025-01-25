4610f4f4d0ffa30ce87408f19ee41be23ba67a37
yekta <ngyekta@gmail.com>
2025-01-25 18:17:50 +0300

    tweak provider

--- src/components/providers/ph-provider.tsx
+++ src/components/providers/ph-provider.tsx
@@ -1,19 +1,17 @@
 "use client";
 
+import posthog from "posthog-js";
 import { env } from "@/lib/env";
 import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 
 export function PhProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <PostHogProvider
-      apiKey={env.NEXT_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: "/ingest",
-        ui_host: env.NEXT_PUBLIC_SITE_URL,
-        person_profiles: "always",
-      }}
-    >
-      {children}
-    </PostHogProvider>
-  );
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: "/ingest",
+      ui_host: env.NEXT_PUBLIC_SITE_URL,
+      person_profiles: "always",
+    });
+  }, []);
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
