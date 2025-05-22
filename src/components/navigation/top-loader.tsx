eab99eb6482c0a21a4558c515a5905706f77ab8a
yekta <ngyekta@gmail.com>
2025-05-22 20:33:25 +0300

    Update nextjs

--- src/components/navigation/top-loader.tsx
+++ src/components/navigation/top-loader.tsx
@@ -2,16 +2,25 @@
 
 import { useTheme } from "next-themes";
 import NextTopLoader from "nextjs-toploader";
+import { useEffect, useState } from "react";
 
 export default function TopLoader() {
   const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setMounted(true);
+    }
+  }, []);
+
   return (
     <NextTopLoader
       zIndex={9999}
       showSpinner={false}
       color="hsl(var(--top-loader))"
       shadow={false}
-      height={resolvedTheme === "dark" ? 2 : 3}
+      height={mounted && resolvedTheme === "dark" ? 2 : 3}
     />
   );
 }
